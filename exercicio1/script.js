9b7bcf9edf7f17b8fc19fcd8fbf5609a2ec796e7
camilareimberg <camila_reimberg@hotmail.com>
2022-07-19 20:43:30 -0300

    exercicio7

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,7 +1,10 @@
 
 let nome = prompt ("Qual o seu nome?")
-let idade= prompt ("Qual a sua idade?")
+let idade= Number(prompt ("Qual a sua idade?"))
+// se não colocar o number antes do prompt ele lê como string
 console.log (typeof nome)
 console.log (typeof idade)
 
+
 console.log ("Olá", nome, "você tem",idade," anos")
+
